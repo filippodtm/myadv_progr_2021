8a4f450ca8ca8e14de472557879e0fa73a35fa4a
filippodtm <99815895+filippodtm@users.noreply.github.com>
2023-03-14 16:06:39 +0100

    Add files via upload

---
+++ c++/provaprova.cpp
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout << "Hello  " << std::endl;
+  std::string nome;
+  unsigned int eta;
+  std::cin >> nome >> eta;
+  std::cout << "hello, " << nome << " [" << eta << "]\n";
+  return 0;
+}
+
+
+
+
+
